e6ee26e685c1e910a7eb83ce751c8a06bbe84e99
Yi Zhang <beingzy@gmail.com>
2017-11-25 22:10:42 -0800

    implement function to plot ROC curve

--- xor/utils.py
+++ xor/utils.py
@@ -46,3 +46,25 @@ def classifier_evaluation(ytrue, ypred):
         "precision_score": precision_score(ytrue, ypred),
         "recall_score": recall_score(ytrue, ypred)
     }
+
+
+def plot_roc(fpr, tpr, color='darkorange'):
+    """plot ROC curve
+    """
+    import matplotlib.pyplot as plt
+    from sklearn.metrics import auc
+
+    auc_score = auc(fpr, tpr)
+
+    fig = plt.figure()
+    plt.plot(fpr, tpr, color=color, lw=2,
+             label='ROC curve (area = %0.2f)' % auc_score)
+    plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver Operating Characteristics (ROC) Curve')
+    plt.legend(loc='lower right')
+
+    return fig
